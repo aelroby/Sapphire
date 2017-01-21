ec22761a9ae137d8afc5341e86067b832217a92b
Ahmed El-Roby <aelroby@andrew.cmu.edu>
2017-01-21 12:37:40 -0500

    Added questions assignment to participants in user study

---
+++ src/ayhay/main/EvaluationAssignment.java
@@ -0,0 +1,38 @@
+package ayhay.main;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import ayhay.utils.FileManager;
+
+public class EvaluationAssignment {
+
+	static int groupNum = 3;
+	
+	static int[] groupQuestionNum = {9, 8, 8};
+	
+	static int questionsNum = 4;
+	
+	public static void main(String[] args) {
+		Set<Integer> chosenSet = new HashSet<Integer>();
+		
+		String contents = "";
+		System.out.println("Assigning questions to participant...");
+		for(int i = 0; i < groupNum; ++i) {
+			chosenSet.clear();
+			for(int j = 0; j < questionsNum; ++j) {
+				int chosenNum = 1 + (int)(Math.random() * groupQuestionNum[i]);
+				while(chosenSet.contains(chosenNum)) {
+					chosenNum = 1 + (int)(Math.random() * groupQuestionNum[i]);
+				}
+				chosenSet.add(chosenNum);
+				contents += Integer.toString(chosenNum) + ") \n";
+			}
+			contents += "=====================\n";
+		}
+		System.out.println(contents);
+		FileManager.writeToFile("ParticipantSession.txt", contents);
+		System.out.println("Done. Find the assignments in ParticipantSession.txt");
+	}
+
+}
